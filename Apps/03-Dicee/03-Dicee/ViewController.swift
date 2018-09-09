16d43cf57a9a778594de1c304a5631d5bc2f8248
Jubeen Shah <jnshah2@ncsu.edu>
2018-09-09 12:32:39 -0400

    update to dicee app
    update to dicee app

--- Apps/03-Dicee/03-Dicee/ViewController.swift
+++ Apps/03-Dicee/03-Dicee/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+   
+    @IBAction func rollButton(_ sender: UIButton) {
+        
+        randomDiceIndex1 = Int.random(in: 0 ... 5)
+        randomDiceIndex2 = Int.random(in: 0 ... 5)
+        
+        print(randomDiceIndex2)
+        print(randomDiceIndex1)
+        
+    }
+    
 }
 
