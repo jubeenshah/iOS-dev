db54be85923aa9d79d51c6f6c9a18b35d321ac2d
Jubeen Shah <jnshah2@ncsu.edu>
2018-09-09 11:23:46 -0400

    03-DiceeApp
    03-DiceeApp

---
+++ Apps/03-Dicee/03-Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  03-Dicee
+//
+//  Created by Jubeen shah on 9/9/18.
+//  Copyright © 2018 Jubeen shah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
