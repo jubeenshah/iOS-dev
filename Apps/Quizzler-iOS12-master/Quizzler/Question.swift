fb290401c0c0823a204c9d8e8c1826df8d507091
Jubeen Shah <jnshah2@ncsu.edu>
2018-09-15 09:54:42 -0400

    Quizzler App
    Quizzler App start

---
+++ Apps/Quizzler-iOS12-master/Quizzler/Question.swift
@@ -0,0 +1,23 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by Jubeen shah on 9/15/18.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Question {
+    
+    let questionText : String
+    let answer : Bool
+    
+    init(text:String, correctAnswer: Bool) {
+        questionText = text
+        answer = correctAnswer
+    }
+    
+    
+    
+}
