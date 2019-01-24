413aa4d7e00999779d70fb5336565fe0ed4f1dca
Jubeen Shah <jnshah2@ncsu.edu>
2019-01-24 16:01:25 -0500

    Commit till codable data storage

--- Apps/10-Todoey/Todoey/Todoey/TodoListViewController.swift
+++ Apps/10-Todoey/Todoey/Todoey/TodoListViewController.swift
@@ -12,27 +12,14 @@ class TodoListViewController: UITableViewController {
 
     var itemArray = [Item]()
     
-    let defaults = UserDefaults.standard
+
+     let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let newItem = Item()
-        newItem.title = "A"
-        itemArray.append(newItem)
-        
-        let newItem2 = Item()
-        newItem2.title = "B"
-        itemArray.append(newItem2)
-        
-        let newItem3 = Item()
-        newItem3.title = "C"
-       
-        
+        loadItems()
 
-        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
-            itemArray = items
-        }
         
     }
 
@@ -57,6 +44,8 @@ class TodoListViewController: UITableViewController {
         //print(itemArray[indexPath.row])
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
+        saveItem()
+        
         tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
         
@@ -67,7 +56,7 @@ class TodoListViewController: UITableViewController {
         
         var textField = UITextField()
         let alert = UIAlertController(title: "Something todoey?", message: "", preferredStyle: .alert)
-        let enterTextAlert = UIAlertController(title: "Please enter Todoey Text please", message: "", preferredStyle: .alert)
+        let enterTextAlert = UIAlertController(title: "Please enter Todoey Text!", message: "", preferredStyle: .alert)
         let dismissEnterTextAlert = UIAlertAction(title: "OK", style: .default) { (dismissEnterTextAlert) in
             self.present(alert, animated: true, completion: nil)
         }
@@ -81,7 +70,7 @@ class TodoListViewController: UITableViewController {
             newItem.title = textField.text!
             if textField.text != "" {
             self.itemArray.append(newItem)
-                self.defaults.set(self.itemArray, forKey: "TodoListArray")
+                self.saveItem()
             } else {
                 
                 self.present(enterTextAlert, animated: true, completion: nil)
@@ -97,5 +86,27 @@ class TodoListViewController: UITableViewController {
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
+    
+    func saveItem() {
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Errot encoding item array, \(error)")
+        }
+    }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+            itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error --> \(error)")
+            }
+        }
+    }
 }
+
 
