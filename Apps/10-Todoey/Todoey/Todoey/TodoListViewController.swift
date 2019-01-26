92d3577db51d5f404c294d047e89ac85036fd9d0
Jubeen Shah <jnshah2@ncsu.edu>
2019-01-26 13:12:18 -0500

    added a search bar with the option to filter. Also added shifted from codable data storage to CoreData

--- Apps/10-Todoey/Todoey/Todoey/TodoListViewController.swift
+++ Apps/10-Todoey/Todoey/Todoey/TodoListViewController.swift
@@ -7,20 +7,23 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoListViewController: UITableViewController {
 
     var itemArray = [Item]()
     
 
-     let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
         loadItems()
 
-        
+        //print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
     }
 
     //MARK - TableView Datasource Methods
@@ -42,8 +45,10 @@ class TodoListViewController: UITableViewController {
     //MARK - TableView Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print(itemArray[indexPath.row])
-        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
+//        context.delete(itemArray[indexPath.row])
+//        itemArray.remove(at: indexPath.row)
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         saveItem()
         
         tableView.reloadData()
@@ -66,8 +71,10 @@ class TodoListViewController: UITableViewController {
             //What will happen once the user clicks the "Add item" button
             //print(textField.text!)
             
-            let newItem = Item()
+
+            let newItem = Item(context: self.context)
             newItem.title = textField.text!
+            newItem.done = false
             if textField.text != "" {
             self.itemArray.append(newItem)
                 self.saveItem()
@@ -88,23 +95,66 @@ class TodoListViewController: UITableViewController {
     }
     
     func saveItem() {
-        let encoder = PropertyListEncoder()
+        
         do {
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+            
+            try context.save()
+            
         } catch {
-            print("Errot encoding item array, \(error)")
+            
+            print("Error saving context -> \(error)")
         }
     }
     
-    func loadItems() {
-        if let data = try? Data(contentsOf: dataFilePath!) {
-            let decoder = PropertyListDecoder()
-            do {
-            itemArray = try decoder.decode([Item].self, from: data)
-            } catch {
-                print("Error --> \(error)")
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
+        
+        do {
+            itemArray = try context.fetch(request)
+        } catch {
+            print("Error Fetching Data \(error)")
+        }
+        
+        tableView.reloadData()
+    }
+    
+}
+
+extension TodoListViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        request.predicate = predicate
+        let sortDiscriptor = NSSortDescriptor(key: "title", ascending: true)
+        request.sortDescriptors = [sortDiscriptor]
+        
+        loadItems(with: request)
+
+    }
+    
+//    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+//
+//    let request : NSFetchRequest<Item> = Item.fetchRequest()
+//    let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+//    request.predicate = predicate
+//    let sortDiscriptor = NSSortDescriptor(key: "title", ascending: true)
+//    request.sortDescriptors = [sortDiscriptor]
+//
+//    do {
+//    itemArray = try context.fetch(request)
+//    } catch {
+//    print("Error Fetching Data \(error)")
+//    }
+//    tableView.reloadData()
+//
+//   }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
             }
+            
         }
     }
 }
